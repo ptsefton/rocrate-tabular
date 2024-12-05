36bbc84a1552e34a9e0a1d47e75daa4420f131ff
Mike Lynch <m.lynch@sydney.edu.au>
2024-12-05 14:17:10 +1100

    Passing http tests

--- tests/test_crates.py
+++ tests/test_crates.py
@@ -3,8 +3,10 @@
 import json
 from pathlib import Path
 from fuzz import random_text, random_property
+from pytest_httpserver import HTTPServer
 
 from rocrate_tabular.tinycrate import TinyCrate, minimal_crate
+
 
 # https://pytest-httpserver.readthedocs.io/en/latest/ for testing fetch
 
@@ -40,3 +42,25 @@ def test_load_file(crates):
         with open(Path(cratedir) / "textfile.txt", "r") as tfh:
             contents2 = tfh.read()
             assert contents == contents2
+
+
+def test_load_url(crates, httpserver: HTTPServer):
+    # test http endpoint with some content
+    contents = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+"""
+    httpserver.expect_request("/textfileonurl.txt").respond_with_data(
+        contents, content_type="text/plain"
+    )
+
+    cratedir = crates["textfiles"]
+    with open(Path(cratedir) / "ro-crate-metadata.json", "r") as jfh:
+        jsonld = json.load(jfh)
+        crate = TinyCrate(jsonld=jsonld, directory=cratedir)
+        # add an entity to the crate with the endpoint URL as the id
+        urlid = httpserver.url_for("/textfileonurl.txt")
+        crate.add("File", urlid, {"name": "textfileonurl.txt"})
+        # get the entity and try to fetch
+        efile = crate.get(urlid)
+        contents2 = efile.fetch()
+        assert contents == contents2
